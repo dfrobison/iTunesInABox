941227743e2fcdb0fde562f062232eda55f06e38
Doug Robison <doug.robison@vivint.com>
2024-10-02 13:44:33 -0600

    Initial Commit

---
+++ iTunesInABox/iTunesInABoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesInABoxApp.swift
+//  iTunesInABox
+//
+//  Created by Doug Robison on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct iTunesInABoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ iTunesInABoxTests/iTunesInABoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  iTunesInABoxTests.swift
+//  iTunesInABoxTests
+//
+//  Created by Doug Robison on 10/2/24.
+//
+
+import Testing
+@testable import iTunesInABox
+
+struct iTunesInABoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
